c8f751b271a7a72032a1333ea25bd4cefbf84bf9
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-09-08 19:03:33 -0300

    Fixing method name

--- work01/timer.c
+++ work01/timer.c
@@ -10,7 +10,7 @@ const float MSEC_FACTOR = 1000.0f;
  * Recebe uma marca de tempo t0 e outra marca de tempo t1 (ambas do tipo struct timeval) 
  * e retorna a diferenca de tempo (delta) entre t1 e t0 em milisegundos (tipo float).
  */
-float timerdifference_msec(
+float timedifference_msec(
     struct timeval t0, 
     struct timeval t1)
 {
