ed7af5a6ff298cda316ecf0d4da0319b0476958a
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-23 22:15:16 -0300

    Implementing set_grid_size() function

--- work04/matrix_lib.cu
+++ work04/matrix_lib.cu
@@ -1,5 +1,13 @@
 #include "matrix_lib.h"
 
+#define THREADS_PER_BLOCK_DEFAULT 256
+#define THREADS_PER_BLOCK 1024
+#define MAX_BLOCKS_PER_GRID_DEFAULT 4096
+#define MAX_BLOCKS_PER_GRID 65535
+
+int blockSize = THREADS_PER_BLOCK_DEFAULT;
+int numBlocks = MAX_BLOCKS_PER_GRID_DEFAULT;
+
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix)
 {
     return SUCESSO;
@@ -12,5 +20,14 @@ int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct ma
 
 int set_grid_size(int threads_per_block, int max_blocks_per_grid)
 {
+    if (threads_per_block > THREADS_PER_BLOCK || 
+        max_blocks_per_grid > MAX_BLOCKS_PER_GRID)
+    {
+        return ERRO;
+    }
+
+    blockSize = threads_per_block;
+    numBlocks = max_blocks_per_grid;
+
     return SUCESSO;
 }
