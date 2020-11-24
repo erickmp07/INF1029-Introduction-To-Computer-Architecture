5a1d6c1f1a4dd3e06b165412009d76bf80ebf5aa
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-23 21:45:02 -0300

    Including matrix_lib header

--- work04/matrix_lib.cu
+++ work04/matrix_lib.cu
@@ -1,22 +1,16 @@
-struct matrix
-{
-    unsigned long int height;
-    unsigned long int width;
-    float *h_rows;
-    float *d_rows;
-};
+#include "matrix_lib.h"
 
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix)
 {
-    return 1;
+    return SUCESSO;
 }
 
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC)
 {
-    return 1;
+    return SUCESSO;
 }
 
 int set_grid_size(int threads_per_block, int max_blocks_per_grid)
 {
-    return 1;
+    return SUCESSO;
 }
