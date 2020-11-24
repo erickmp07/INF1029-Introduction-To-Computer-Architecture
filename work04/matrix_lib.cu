845c5c3a6f0f089f9b8f1f71df5a93d0cef3454f
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2020-11-23 21:20:35 -0300

    Declaring matrix_matrix_mult() function

--- work04/matrix_lib.cu
+++ work04/matrix_lib.cu
@@ -10,3 +10,8 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix)
 {
     return 1;
 }
+
+int matrix_matrix_mult(struct matrix *matrixA, struct matrix *matrixB, struct matrix *matrixC)
+{
+    return 1;
+}
